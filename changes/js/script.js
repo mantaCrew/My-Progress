909639b68e20652a16a0020bf03be7146737f2fc
Manta Crew <themantacrew@gmail.com>
2023-12-15 17:02:00 -0800

    Keeping the routine
    Using github daily!

--- changes/js/script.js
+++ changes/js/script.js
@@ -45,7 +45,7 @@ const btn = document.querySelector("button");
 const txt = document.querySelector("p");
 console.log(btn);
 
-// btn.addEventListener("click", updateBtn); this is redunant?
+btn.addEventListener("click", updateBtn); this is redunant?
 
 function updateBtn() {
     if (btn.textContent === "Start machine") {
