6da66270ba56059e8d3e8f72bf97a9781ab6ab68
Manta Crew <themantacrew@gmail.com>
2023-12-16 15:38:07 -0800

    Using this for the Odin Project
    I don't want the live server situation to console.log and alert
    every single thing I am doing. So starting fresh!

--- changes/js/script.js
+++ changes/js/script.js
@@ -1,12 +1,12 @@
 let bet = 10;
 
-alert(typeof(bet));
+//alert(typeof(bet));
 
 let bet1 = 'String';
 
-alert(bet1);
+//alert(bet1);
 
-alert(typeof(bet1));
+//alert(typeof(bet1));
 
 
 // console.log("You","suck!")
@@ -16,16 +16,16 @@ let admin
 let name = 'John';
 admin = name;
 
- alert(admin);
+ //alert(admin);
 
 // Could also be done this way
 
 // let admin, name;
 name = "John";
 admin + name;
-alert(admin);
+//alert(admin);
 
-alert(10 / 2);
+//alert(10 / 2);
 
 // getting rid of decimals
 
@@ -33,17 +33,17 @@ const lotsOfDecimal = 1.766584958675746364;
 lotsOfDecimal;
 const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
 twoDecimalPlaces;
-console.log(twoDecimalPlaces);
+//console.log(twoDecimalPlaces);
 
 //— passing the string value into the Number() constructor to return a number version of the same value.
 
 let myNumber = "74";
 myNumber = Number(myNumber) + 3;
-console.log(myNumber);
+//console.log(myNumber);
 
 const btn = document.querySelector("button");
 const txt = document.querySelector("p");
-console.log(btn);
+//console.log(btn);
 
 btn.addEventListener("click", updateBtn); // this is redunant?
 
@@ -60,7 +60,7 @@ function updateBtn() {
   // back ticks ` are for template literals
 name = "Chris";
 const greeting = `Hello, ${name}`;
-console.log(greeting); // "Hello, Chris" 
+//console.log(greeting); // "Hello, Chris" 
 
 // This is known as concantenation?
 // Adding strings together
@@ -68,7 +68,7 @@ console.log(greeting); // "Hello, Chris"
 const one = "Hello, ";
 const two = "how are you?";
 const joined = `${one}${two}`;
-console.log(joined); // "Hello, how are you?"
+//console.log(joined); // "Hello, how are you?"
 
 //Concatenation in context 
 
@@ -87,7 +87,7 @@ button.addEventListener("click", greet);
 
 let linebreak = "What the fuck\nare you doing?";
 
-console.log(linebreak);
+//console.log(linebreak);
 //What the fuck
 //are you doing?
 
@@ -95,8 +95,8 @@ console.log(linebreak);
 // this \ right before makes sure the script is ignored.
 
 let quote = 'It\'s okay, I said, "This is okay!"';
-console.log(quote);
+// console.log(quote);
 
-console.log(Number(quote));
+// console.log(Number(quote));
 
 // Semi wanting to start over, what do I do?
