bb5f9689609f7f05aa157d8e57f1257481c110b0
Manta Crew <themantacrew@gmail.com>
2023-12-18 18:13:30 -0800

    Reference Add
    Typing some code.

--- changes/js/script.js
+++ changes/js/script.js
@@ -164,9 +164,17 @@ console.log(typeof(NaN));
 }
 */
 
- if (hour < 20) {
+if (hour < 20) {
   greeting = "Good day";
 } else {
   greeting = "Good evening";
 }
+console.log(greeting);
 
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
