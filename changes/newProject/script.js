4bcbe08f63db5ded304f214d82b70256d20da57c
Manta Crew <themantacrew@gmail.com>
2023-12-23 17:47:14 -0800

    Linked JS file
    This will be the start of my odin project calculator! Wish me luck :D

---
+++ changes/newProject/script.js
@@ -0,0 +1,7 @@
+/* my goal here is to begin the calculator project as I learn code.
+
+I know that I am not there yet, however as I learn I can begin on 
+
+creating the frame and structure of my calculator. */
+
+alert("Calculator, here I come!");
